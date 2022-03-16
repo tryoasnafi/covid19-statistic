c118059d5d231add6b1d4542a54c97bc9a987439
Tryo Asnafi <tryoasnafi@gmail.com>
2022-03-16 22:17:00 +0700

    update webpack

--- webpack.common.js
+++ webpack.common.js
@@ -2,23 +2,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: "./src/app.js",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"]
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-            filename: "index.html"
-        })
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader']
+      }
     ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: './src/images/coronavirus.svg',
+      template: './src/template.html',
+      filename: 'index.html'
+    })
+  ]
 }
